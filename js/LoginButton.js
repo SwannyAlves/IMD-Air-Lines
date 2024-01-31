382c1eed8a496f69f7c7fcb4c9b0bf652fab19ec
Swanny Alves <swannykathllen@gmail.com>
2024-01-31 00:53:43 -0300

    style: customizando a loginPage

--- js/LoginButton.js
+++ js/LoginButton.js
@@ -1,21 +1,21 @@
-function loginAdmin(){
-    var login = document.getElementById('emailLogin').value;
-    var password = document.getElementById('passwordLogin').value;
+function loginAdmin() {
+  var login = document.getElementById("emailLogin").value
+  var password = document.getElementById("passwordLogin").value
 
-    if(login === "admin") {
-        if(password === "admin"){
-            redirectToAdminPage();
-        } else {
-            window.alert("Login ou senha incorreto");
-        }
+  if (login === "admin") {
+    if (password === "admin") {
+      redirectToAdminPage()
     } else {
-        window.alert("Login ou senha incorreto");
+      window.alert("Login ou senha incorreto")
     }
+  } else {
+    window.alert("Login ou senha incorreto")
+  }
 }
 
-function redirectToAdminPage(){
-    console.log("Cheguei");
-    //Resolver o problema de encaminhamento para a página do administrador
-    window.location.href = "../html/AdminPage.html";
-    console.log("Saindo");
+function redirectToAdminPage() {
+  console.log("Cheguei")
+  //Resolver o problema de encaminhamento para a página do administrador
+  window.location.href = "adminPage.html"
+  console.log("Saindo")
 }
